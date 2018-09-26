28ef31668f930ce45d1e7daad8753f1fc423b24e
daniel-deutsch <daniel.deutsch@polijunior.com.br>
2018-09-26 00:53:35 -0300

    agr foi

--- EP1/main.py
+++ EP1/main.py
@@ -1,6 +1,7 @@
 # Importação de bibliotecas do python
 import matplotlib.pyplot as plt
 import numpy as np
+np.set_printoptions(threshold=np.inf)
 
 # Importação de funções de outras páginas
 from funcoes_auxiliares import *
@@ -13,12 +14,14 @@ Vinterna = 100                                               # Potenciais das pl
 
 
 # Define-se o tamanho da matriz de potenciais (quanto maior maior a precisão) e as variáveis que dependem disso
-precisao = 10
+precisao = 1
 
 matriz_de_potenciais = cria_matriz_de_potenciais(Vinterna, a, b, c, d, g, h, precisao)
 m, n = matriz_de_potenciais.shape[0], matriz_de_potenciais.shape[1]
 deltaX, deltaY = a / (n - 1), b / (m - 1)
 
+matriz_de_potenciais, lista_alteracoes = calcula_potencial(matriz_de_potenciais, Vinterna)
+while len(lista_alteracoes) != 0:
+    matriz_de_potenciais, lista_alteracoes = calcula_potencial(matriz_de_potenciais, Vinterna)
 
-
-
+print(matriz_de_potenciais)
