6305a6384fc6bf4b3aaf5aeacde94671f570d485
Pedro Maronezi <pedro.maronezi@polijunior.com.br>
2018-09-27 20:57:33 -0300

    fix nas matrizes de potenciais + cria matriz dual e calcula potencial da matriz dual

--- EP1/funcoes_auxiliares.py
+++ EP1/funcoes_auxiliares.py
@@ -8,7 +8,11 @@ def cria_matriz_de_potenciais(Vinterna, a, b, c, d, g, h, precisao):
 
     for i in range(m - int(h*1E2 + d*1E2) * precisao, m - int(h*1E2) * precisao):
         for j in range(int(g*1E2) * precisao + 1, int(g*1E2 + c*1E2) * precisao + 1):
-            matriz_de_potenciais[i][j] = Vinterna
+            #Dá uma olhada nisso, aqui o meio fica com none (= null) e só as bordas ficam com Vinterno
+            if i == m - int(h*1E2 + d*1E2) * precisao or i == m - int(h*1E2) * precisao or j == int(g*1E2) * precisao + 1 or j == int(g*1E2 + c*1E2) * precisao + 1:
+                matriz_de_potenciais[i][j] = Vinterna
+            else:
+                matriz_de_potenciais[i][j] = None
             
     return matriz_de_potenciais
 
@@ -52,4 +56,42 @@ def matrizes_de_campo_eletrico(matriz_de_potenciais, Vinterna, deltaX, deltaY):
     return matriz_campo_eletrico_x, matriz_campo_eletrico_y 
 
 #def calcula_resistencia():
-    
+
+def cria_matriz_dual_de_potenciais(Vinterna, a, b, c, d, g, h, precisao):
+    m, n = int(precisao * (b / 2 * 1E2)) + 1, int(precisao * (a * 1E2)) + 1  # Dimensões da matriz
+
+    matriz_dual_de_potenciais = np.zeros((m, n))
+
+    for i in range(m - int(d/2 * 1E2) * precisao, m):
+        for j in range(int(g * 1E2) * precisao + 1, int(g * 1E2 + c * 1E2) * precisao + 1):
+            # Dá uma olhada nisso, aqui o meio fica com none (= null) e só as bordas ficam com Vinterno
+            if i == m - int(d/2 * 1E2) * precisao or j == int(g * 1E2) * precisao + 1 or j == int(g * 1E2 + c * 1E2) * precisao + 1:
+                matriz_dual_de_potenciais[i][j] = Vinterna
+            else:
+                matriz_dual_de_potenciais[i][j] = None
+
+    return matriz_dual_de_potenciais
+
+def calcula_potencial_dual(matriz_dual_de_potenciais, Vinterna):
+    m, n = matriz_dual_de_potenciais.shape[0], matriz_dual_de_potenciais.shape[1]
+
+    lista_alteracoes = []
+
+    for i in range(m):
+        for j in range(n):
+            if i != 0 and i != m - 1 and j != 0 and j != n - 1 and matriz_dual_de_potenciais[i][j] != Vinterna:
+
+                valor = (matriz_dual_de_potenciais[i - 1][j] + matriz_dual_de_potenciais[i + 1][j] + matriz_dual_de_potenciais[i][j - 1] +
+                    matriz_dual_de_potenciais[i][j + 1]) / 4
+
+                try:
+                    if abs(matriz_dual_de_potenciais[i][j] - valor) / matriz_dual_de_potenciais[i][j] > 0.001:
+                        lista_alteracoes.append((i, j, valor))
+                        matriz_dual_de_potenciais[i][j] = valor
+                except ZeroDivisionError:
+                    continue
+
+    for alteracao in lista_alteracoes:
+        matriz_dual_de_potenciais[alteracao[0]][alteracao[1]] = alteracao[2]
+
+    return matriz_dual_de_potenciais, lista_alteracoes
