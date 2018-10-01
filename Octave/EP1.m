9dbdf677c0fe54ed5d2f7ae9331c07e71270afc5
Pedro Maronezi <pedro.maronezi@polijunior.com.br>
2018-09-30 23:42:06 -0300

    EP pronto mas não 100% perfeito

--- Octave/EP1.m
+++ Octave/EP1.m
@@ -60,7 +60,7 @@ for i = Y1:Y2
 end
 
 %Calculo de Potenciais
-for i=1:500
+for i=1:5000
   
   %Calculo do Potencial no retângulo superior
   for i = (Y2 + 1):(m - 1)    
@@ -116,7 +116,7 @@ for i = Y1:Y2
 end
 
 %Calculo de metade do potencial dual
-for i=1:500
+for i=1:5000
     for i = ((Y1 + Y2) / 2) + 1:m
       
       for j = 1:X1
@@ -181,11 +181,78 @@ for i = 1:(((Y1 + Y2) / 2) - 1)
   end
 end
 
+
+%Densidade de carga minima
+%Extremo Superior
+for j = 1:n
+  i = 1;
+  densQ = -eps*(M(i+1,j) - M(i,j))/div;
+  if densQ < densQmin
+    densQmin = densQ;
+  end
+end
+
+%Extremo Inferior
+for j = 1:n
+  i = m - 1;
+  densQ = eps*(M(i+1,j) - M(i,j))/div;
+  if densQ < densQmin
+    densQmin = densQ;
+  end
+end
+
+%Lateral Esquerda
+for i = 1:m
+  j = 1;
+  densQ = eps*(M(i,j) - M(i,j+1))/div;
+  if densQ < densQmin
+    densQmin = densQ;
+  end
+end
+
+%Lateral Direita
+for i = 1:m
+  j = n - 1;
+  densQ = -eps*(M(i,j) - M(i,j+1))/div;
+  if densQ < densQmin
+    densQmin = densQ;
+  end
+end
+
+%RESISTENCIA E CAPACITÂNCIA
+%Cálculo da Integral Numérica do Campo Elétrico
+soma = 0;
+
+%Extremo Superior
+for j = 2:(n-1)
+  soma = soma + M(2,j);
+end
+
+%Extremo Inferior
+for j = 2:(n-1)
+  soma = soma + M((m-1),j);
+end
+
+%Lateral Esquerda
+for i = 2:(m-1)
+  soma = soma + M(i,2);
+end
+
+%Lateral Direita
+for i = 2:(m-1)
+  soma = soma + M(i,(n-1));
+end
+
+FluxoCampoE = soma * espessura;
+FluxoCampoEDual = espessura * (D(1+(Y1 + Y2)/2,1)/2 + sum(D(1+(Y1 + Y2)/2,2:X1-1)) + D(1+(Y1 + Y2)/2,X1)/2);
+RLinhaNumerico = Vint/(sigma2*FluxoCampoEDual);
+R = Vint/(sigma1*FluxoCampoE);
+RLinhaDual = 1/(2*R*sigma1*(espessura.^2)*sigma2);
+C = eps*FluxoCampoE/Vint;
+
 %Plotando o mapa de quadrados curvilineos
 figure(1);
 axis equal
 contour(M, [0 10 20 30 40 50 60 70 80 90 100]);
 hold on
 contour(D, 40);
-
-%Densidade de carga minima
